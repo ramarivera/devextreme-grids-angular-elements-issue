372b73639a0aaf4f9b6e143557f38b3d19d62a98
rrivera <ramarivera@gmail.com>
2019-11-26 10:21:09 -0300

    as bootstrap array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA, Injector, DoBootstrap } from '@angular/core';
-import { createCustomElement } from '@angular/elements';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GridsModule } from './grids/grids.module';
-import { GenericGridComponent } from './grids/generic-grid/generic-grid.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [GridsModule, BrowserModule],
   providers: [],
-  entryComponents: [GenericGridComponent]
+  bootstrap: [AppComponent]
 })
-export class AppModule implements DoBootstrap {
-  constructor(private injector: Injector) {}
-
-  ngDoBootstrap() {
-    const el = createCustomElement(GenericGridComponent, { injector: this.injector });
-    customElements.define('mpm-data-grid', el);
-    console.log(el);
-  }
-}
+export class AppModule {}
